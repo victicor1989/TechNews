cd7dab607ae6ac78eeda34d9b1617021e426854b
Victor Aristizabal <varistizabal1730@gmail.com>
2022-11-26 10:38:26 -0500

    Se corrigen errores en el codigo y se agrega un boton de reload con JS

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let botonReload = document.querySelector('.nav-brand')
+botonReload.addEventListener('click', reloadPage)
+
 document.querySelector('.menu-btn').addEventListener('click', () => {
   document.querySelector('.nav-menu').classList.toggle('show');
 });
@@ -6,3 +9,7 @@ ScrollReveal().reveal('.showcase');
 ScrollReveal().reveal('.news-cards' , {delay: 500});
 ScrollReveal().reveal('.cards-banner-one' , {delay: 500});
 ScrollReveal().reveal('.cards-banner-two' , {delay: 500});
+
+function reloadPage() {
+  location.reload()
+}
